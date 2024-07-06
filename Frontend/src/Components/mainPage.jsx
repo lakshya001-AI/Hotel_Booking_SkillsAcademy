10d597596e49645a192e04683ed4137aa86b8bf0
Lakshya Makode <makodelakshya101@gmail.com>
2024-07-06 19:02:11 +0530

    commit changes

--- Frontend/src/Components/mainPage.jsx
+++ Frontend/src/Components/mainPage.jsx
@@ -5,7 +5,9 @@ export default function MainPage() {
   return <>
   <div className={Style.mainDivMainPage}>
     {/* Navbar section that will contain the login and create account and the user Profile (if the user successful login )*/}
-    <div className={Style.navBar}></div>
+    <div className={Style.navBar}>
+      
+    </div>
     {/* Booking Data section and Search Button to search for the Hotels */}
     {/* Hotel List based on the user data selected */}
   </div>

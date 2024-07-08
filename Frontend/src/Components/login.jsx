4ab4f806cfbc67f27c22389f3f6469026ad64a2d
Lakshya Makode <makodelakshya101@gmail.com>
2024-07-09 01:08:33 +0530

    commit changes

--- Frontend/src/Components/login.jsx
+++ Frontend/src/Components/login.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import Style from "../App.module.css";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
-  return<>
-    <div className={Style.mainDivMainPage}>
-      <div className={Style.loginPageMainDiv}>
-        
-
+  return (
+    <>
+      <div className={Style.mainDivMainPage}>
+        <div className={Style.loginPageMainDiv}>
+          <div className={Style.loginPageDiv}>
+            <div className={Style.loginPageDiv1}>
+              <div className={Style.overlay1}>
+              <p className={Style.loginPara1}>Welcome to login</p>
+              <p className={Style.loginPara2}>Don't have an account?</p>
+              <Link className={Style.createAccountBtn} to="/createAccount">Create Account</Link>
+              </div>
+            </div>
+            <div className={Style.loginPageDiv2}></div>
+          </div>
+        </div>
       </div>
-    </div>
-  </>
+    </>
+  );
 }
 
 export default LoginPage;

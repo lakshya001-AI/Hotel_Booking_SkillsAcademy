7096d4d828d9dc4e9f02249d62b57abdd9138afb
Lakshya Makode <makodelakshya101@gmail.com>
2024-07-09 21:47:31 +0530

    commit changes

--- Frontend/src/Components/login.jsx
+++ Frontend/src/Components/login.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Style from "../App.module.css";
 import { Link } from "react-router-dom";
 
 function LoginPage() {
+
+  const[email, setEmail] = useState();
+  const[password, setPassword] = useState();
+
+  function getData(){
+    let obj = {
+      email:email,
+      password:password
+    }
+
+    console.log(obj);
+  }
   return (
     <>
       <div className={Style.mainDivMainPage}>
@@ -15,7 +27,23 @@ function LoginPage() {
               <Link className={Style.createAccountBtn} to="/createAccount">Create Account</Link>
               </div>
             </div>
-            <div className={Style.loginPageDiv2}></div>
+            <div className={Style.loginPageDiv2}>
+              <p className={Style.signInPara}>Sign In</p>
+              <div className={Style.emailInputDiv}>
+                <p>Email</p>
+                <input type="email" className={Style.emailInputField} placeholder="hbooking@gmail.com" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+              </div>
+              <div className={Style.passwordInputDiv}>
+                <p>Password</p>
+                <input type="password" className={Style.emailInputField} placeholder="*************" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+              </div>
+              <div className={Style.signInBtnInputDiv}>
+              <button className={Style.signInBtn} onClick={getData}>Sign In</button>
+              </div>
+
+             
+
+            </div>
           </div>
         </div>
       </div>

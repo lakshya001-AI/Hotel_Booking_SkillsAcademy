3ba18ca5cff94124e502056031aadf150cf14cee
Lakshya Makode <makodelakshya101@gmail.com>
2024-07-09 21:55:32 +0530

    commit changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./Components/mainPage";
 import LoginPage from "./Components/login";
+import CreateAccount from "./Components/createAccount";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
           {/* Here we are going to have various routes */}
           <Route path="/" element={<MainPage/>}/>
           <Route path="/loginPage" element={<LoginPage/>}/>
+          <Route path="/createAccount" element={<CreateAccount/>}/>
         </Routes>
       </BrowserRouter>
     </>
---
+++ Frontend/src/Components/createAccount.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import Style from "../App.module.css";
+import { Link } from "react-router-dom";
+
+function CreateAccount() {
+    const[email, setEmail] = useState();
+    const[password, setPassword] = useState();
+  
+    function getData(){
+      let obj = {
+        email:email,
+        password:password
+      }
+  
+      console.log(obj);
+    }
+  return <>
+  <div className={Style.mainDivMainPage}>
+        <div className={Style.loginPageMainDiv}>
+          <div className={Style.loginPageDiv}>
+            <div className={Style.loginPageDiv1}>
+              <div className={Style.overlay1}>
+              <p className={Style.loginPara1}>Welcome to login</p>
+              <p className={Style.loginPara2}>Don't have an account?</p>
+              <Link className={Style.createAccountBtn} to="/createAccount">Create Account</Link>
+              </div>
+            </div>
+            <div className={Style.loginPageDiv2}>
+              <p className={Style.signInPara}>Sign In</p>
+              <div className={Style.emailInputDiv}>
+                <p>Email</p>
+                <input type="email" className={Style.emailInputField} placeholder="hbooking@gmail.com" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+              </div>
+              <div className={Style.passwordInputDiv}>
+                <p>Password</p>
+                <input type="password" className={Style.emailInputField} placeholder="*************" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+              </div>
+              <div className={Style.signInBtnInputDiv}>
+              <button className={Style.signInBtn} onClick={getData}>Sign In</button>
+              </div>
+
+             
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+  </>
+}
+
+export default CreateAccount;

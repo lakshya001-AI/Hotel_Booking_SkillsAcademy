c37fd82707cdde9642025257741d4c066ad57496
Lakshya Makode <makodelakshya101@gmail.com>
2024-07-10 18:47:35 +0530

    commit changes

--- Frontend/src/Components/adminPage.jsx
+++ Frontend/src/Components/adminPage.jsx
@@ -6,8 +6,16 @@ function AdminPage() {
 
   <div className={Style.mainDivMainPage}>
     <div className={Style.adminMainDiv}>
-      <h3 className={Style.adminPara1}>Welcome to Admin Page</h3>
-      
+      <div className={Style.adminNavbar}>
+      <h3 className={Style.adminPara1}>Admin Panel</h3>
+
+      <div className={Style.adminNavButtonDiv}>
+        <button className={Style.addHotelBtn}>Add Hotel</button>
+        <button className={Style.usersBtn}>Users</button>
+      </div>
+
+
+      </div>
 
     </div>
 

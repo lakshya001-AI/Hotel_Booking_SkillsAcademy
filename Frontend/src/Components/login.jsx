52e3472b26acf1fab95c6b0c85b46c7e703bc1de
Lakshya Makode <makodelakshya101@gmail.com>
2024-07-10 17:12:08 +0530

    commit changes

--- Frontend/src/Components/login.jsx
+++ Frontend/src/Components/login.jsx
@@ -4,7 +4,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Link } from "react-router-dom";
 import axios from "axios";
-
+``
 function LoginPage() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();

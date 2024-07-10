ee9ebc1e3fe90a9ee13e0b81d656c0eb84f6c2ad
Lakshya Makode <makodelakshya101@gmail.com>
2024-07-10 18:29:41 +0530

    commit change

--- Frontend/src/Components/adminPage.jsx
+++ Frontend/src/Components/adminPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Style from "../App.module.css"
 
 function AdminPage() {
   return <>
-  
+
+  <div className={Style.mainDivMainPage}>
+    
+  </div>
+
+
   
   </>
 }

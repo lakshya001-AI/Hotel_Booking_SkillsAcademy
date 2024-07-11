5db5541529eedccc32583ef7079b62644639b2ef
Lakshya Makode <makodelakshya101@gmail.com>
2024-07-12 01:12:17 +0530

    commit changes

--- Frontend/src/Components/adminusers.jsx
+++ Frontend/src/Components/adminusers.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Style from "../App.module.css"
 import { Link } from 'react-router-dom';
+import axios from "axios";
 
 function AdminUsersPage() {
+
+  // We are going to fetch the users from the users database and show the users here
+
+  const [users, setUsers] = useState([]);
+
+  
+  useEffect(()=>{
+    async function getData(){
+      axios.get("http://localhost:5000/getUsersDetails")
+      .then((res)=>{
+        if(res.status === 200){
+          setUsers(res.data);
+        }
+      })
+      .catch((error)=>{
+        toast.error("Error in getting the users data", {
+          position: "top-center",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: false,
+          progress: undefined,
+          theme: "colored",
+        });
+
+      });
+    }
+    getData();
+  },[]);
+
   return <>
 
   <div className={Style.mainDivMainPage}>
@@ -18,8 +50,17 @@ function AdminUsersPage() {
 
       </div>
 
+      <div className={Style.usersList}>
+      {
+        users.map((user) => (
+          <div key={user._id} className={Style.usersDiv1}>
+            <p>{user.name}</p>
+          </div>
+        ))
+      }
     </div>
 
+  </div>
   </div>
 
 
